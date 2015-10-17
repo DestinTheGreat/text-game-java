dcc1583b809f16029130fe2525f8593dcb3bda18
destinthegreat <destinthegreat@vivaldi.net>
2015-10-17 13:51:59 -0500

    Formatting

--- src/main.java
+++ src/main.java
@@ -3,31 +3,32 @@ public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int getInt = IO.getInt("Enter:\n 8. For moving Forward\n 2. "
-				+ "For moving backwards\n "
+		int getInt = IO.getInt("Enter:\n 8. For moving Forward\n 2. " + "For moving backwards\n "
 				+ "4. ForGoing left\n 6. For Going right");
 		String movementMethod = movement(getInt);
 		output(getInt, movementMethod);
 	}
-	public static String movement(double g){
+
+	public static String movement(double g) {
 		if (g == 8)
 			return "You move Forward";
-		
+
 		else if (g == 6)
 			return "You move to the right";
-		
+
 		else if (g == 4)
-			return "You move to the left"; 
-		
+			return "You move to the left";
+
 		else if (g == 2)
 			return "You move backwards";
-		
+
 		else
 			return "Please Enter the Valid Numbers";
 
 	}
+
 	public static void output(double g, String m) {
 		System.out.print(m);
 	}
-	
+
 }
