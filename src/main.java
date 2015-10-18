b472d7e36f9909e92d80ed999c8de951a12309c8
destinthegreat <destinthegreat@vivaldi.net>
2015-10-18 14:41:59 -0500

    Removed Crap

--- src/main.java
+++ src/main.java
@@ -2,7 +2,6 @@
 public class main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		int getInt = IO.getConsoleInt("Enter:\n 8. For moving Forward\n 2. " + "For moving backwards\n "
 				+ "4. For Going left\n 6. For Going right " + "\n\t Enter Number Here ");
 		String movementMethod = movement(getInt);
