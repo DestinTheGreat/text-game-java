7a7eed0a5017ad3fde2bf91163b679bc95254ced
destinthegreat <destinthegreat@vivaldi.net>
2015-10-18 22:18:43 -0500

    Major Revamp

--- src/main.java
+++ src/main.java
@@ -1,42 +1,55 @@
+import java.util.Scanner;
 
 public class main {
 
 	public static void main(String[] args) {
-		int getInt = IO.getConsoleInt("Enter:\n 8. For moving Forward\n 2. " + "For moving backwards\n "
-				+ "4. For Going left\n 6. For Going right " + "\n\t Enter Number Here ");
-		String movementMethod = movement(getInt);
-		output(getInt, movementMethod);
+		System.out.println("Enter:\n 8: For moving Forward\n 2: " + "For moving backwards\n "
+				+ "4: For Going left\n 6: For Going right ");
+		int getInt = IO.getConsoleInt("\n\tEnter Number Here ");
+		int checkedans = checker(getInt);
+
+		String movementresult = movement(checkedans);
+		
+		System.out.println(movementresult);
+		
+		while(movementresult != "\nYou moved nowhere!"){
+			getInt = -250253540;
+			checkedans = checker(getInt);
+			checkedans = checker(checkedans);
+			movementresult = movement(checkedans);
+			System.out.println(movementresult);
+		}
 	}
-
-	public static String movement(double g) {
-	
-		if (g == 8)
-			return "You move Forward";
-
-		else if (g == 6)
-			return "You move to the right";
-
-		else if (g == 4)
-			return "You move to the left";
-
-		else if (g == 2)
-			return "You move backwards";
-
-		else
-			return "Please Enter the Valid Number!";
-
+	public static int checker(int getInt) {
+		while(getInt != 8 && getInt != 2 && getInt != 4 && getInt != 6 && getInt != -250253540)
+		{
+			System.out.println("Error");
+			getInt = 0;
+			getInt = IO.getConsoleInt("\n\tEnter Number Here ");
+		}
+		while(getInt == -250253540){
+			getInt = IO.getConsoleInt("\n\tEnter Number Here ");
+		}
+		
+		return getInt;
+		
 	}
-	public static void output(int inputint, String inputString) {
+	public static String movement(int inputint) {
 		if (inputint == 8)
-			System.out.print(inputString);
+			return "\nYou move Forward";
+
 		else if (inputint == 6)
-			System.out.print(inputString);
+			return "\nYou move to the right";
+
 		else if (inputint == 4)
-			System.out.print(inputString);
+			return "\nYou move to the left";
+
 		else if (inputint == 2)
-			System.out.print(inputString);
+			return "\nYou move backwards";
+
 		else
-			System.out.print("You done goofed! Pick a right number!");
+			return "\nYou moved nowhere!";
+
 	}
 
 }
