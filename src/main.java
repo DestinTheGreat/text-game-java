86dc25821dc174ca34e8abbd440c0cb166bd54d4
destinthegreat <destinthegreat@vivaldi.net>
2015-10-19 23:26:53 -0500

    Made Global Variables

--- src/main.java
+++ src/main.java
@@ -1,10 +1,11 @@
-import java.util.Scanner;
-
 public class main {
-
+	static int left = 0;
+	static int right = 0;
+	static int back = 0;
+	static int forward = 0;
 	public static void main(String[] args) {
 		System.out.println("Enter:\n 8: For moving Forward\n 2: " + "For moving backwards\n "
-				+ "4: For Going left\n 6: For Going right ");
+				+ "4: For Going left\n 6: For Going right \n ");
 		int getInt = IO.getConsoleInt("\n\tEnter Number Here ");
 		int checkedans = checker(getInt);
 
@@ -36,21 +37,25 @@ public class main {
 
 	}
 	public static String movement(int inputint) {
-		if (inputint == 8)
+		if (inputint == 8){
+			forward++;
 			return "\nYou move Forward";
-
-		else if (inputint == 6)
+		}
+		else if (inputint == 6){
+			right++;
 			return "\nYou move to the right";
-
-		else if (inputint == 4)
+		}
+		else if (inputint == 4){
+			left++;
 			return "\nYou move to the left";
-
-		else if (inputint == 2)
+		}
+		else if (inputint == 2){
+			back++;
 			return "\nYou move backwards";
-
-		else
+		}
+		else{
 			return "\nYou moved nowhere!";
-
+		}
 	}
 
 }
