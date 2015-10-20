14fec34b562851ce98370350919e001aa46b9f11
destinthegreat <destinthegreat@vivaldi.net>
2015-10-20 12:01:33 -0500

    int to double

--- src/main.java
+++ src/main.java
@@ -1,59 +1,59 @@
 public class main {
-	static int left = 0;
-	static int right = 0;
-	static int back = 0;
-	static int forward = 0;
-	static int movementnumber = 0;
+	static double left = 0;
+	static double right = 0;
+	static double back = 0;
+	static double forward = 0;
+	static double movementnumber = 0;
 	public static void main(String[] args) {
 		System.out.println("Enter:\n 8: For moving Forward\n 2: " + "For moving backwards\n "
 				+ "4: For Going left\n 6: For Going right \n ");
-		int getInt = IO.getConsoleInt("\n\tEnter Number Here ");
-		int checkedans = checker(getInt);
+		double inputDouble = IO.getConsoleDouble("\n\tEnter Number Here ");
+		double checkedans = checker(inputDouble);
 
 		String movementresult = movement(checkedans);
 
 		System.out.println(movementresult);
 
 		while(movementresult != "\nYou moved nowhere!"){
-			getInt = -250253540;
-			checkedans = checker(getInt);
+			inputDouble = -250253540;
+			checkedans = checker(inputDouble);
 			checkedans = checker(checkedans);
 			movementresult = movement(checkedans);
 			System.out.println(movementresult);
 		}
 
 	}
-	public static int checker(int getInt) {
-		while(getInt != 8 && getInt != 2 && getInt != 4 && getInt != 6 && getInt != -250253540)
+	public static double checker(double inputDouble) {
+		while(inputDouble != 8 && inputDouble != 2 && inputDouble != 4 && inputDouble != 6 && inputDouble != -250253540)
 		{
 			System.out.println("Error");
-			getInt = 0;
-			getInt = IO.getConsoleInt("\n\tEnter Number Here ");
+			inputDouble = 0;
+			inputDouble = IO.getConsoleDouble("\n\tEnter Number Here ");
 		}
-		while(getInt == -250253540){
-			getInt = IO.getConsoleInt("\n\tEnter Number Here ");
+		while(inputDouble == -250253540){
+			inputDouble = IO.getConsoleDouble("\n\tEnter Number Here ");
 		}
 
-		return getInt;
+		return inputDouble;
 
 	}
-	public static String movement(int inputint) {
-		if (inputint == 8){
+	public static String movement(double checkedans) {
+		if (checkedans == 8){
 			forward++;
 			movementnumber++;
 			return "\nYou move Forward";
 		}
-		else if (inputint == 6){
+		else if (checkedans == 6){
 			right++;
 			movementnumber++;
 			return "\nYou move to the right";
 		}
-		else if (inputint == 4){
+		else if (checkedans == 4){
 			left++;
 			movementnumber++;
 			return "\nYou move to the left";
 		}
-		else if (inputint == 2){
+		else if (checkedans == 2){
 			back++;
 			movementnumber++;
 			return "\nYou move backwards";
